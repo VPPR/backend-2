b090a0197397a9b1ed5c6038483c4000ade746af
Pratik Gorade <pratikgorade0@gmail.com>
2021-03-10 22:25:35 +0530

    created model classes for zip files currently all datatypes for date and time are set to stringfield which is going to be replaced by unixtime later
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

---
+++ app/models/zip.py
@@ -0,0 +1,41 @@
+from mongoengine import Document
+from mongoengine.fields import FloatField, IntField, LongField, StringField
+from typing_extensions import Required
+
+class Activity(Document):
+    date = StringField(required=True)
+    steps = IntField(required=True)
+    distance = IntField(required=True)
+    run_distance = IntField(required=True)
+    calories = IntField(required=True)
+
+class Sleep(Document):
+    date = StringField(required=True)
+    deep_sleep_time = IntField(required=True)
+    shallow_sleep_time = IntField(required=True)
+    wake_time = IntField(required=True)
+    sleep_start_time = LongField(required=True)
+    sleep_stop_time = LongField(required=True)
+
+class Sport(Document):
+    sport_type = IntField(required=True)
+    start_time = LongField(required=True)
+    sport_time = IntField(required=True)
+    distance = FloatField(required=True)
+    max_pace = FloatField(required=True)
+    min_pace = FloatField(required=True)
+    avg_pace = FloatField(required=True)
+    calories = IntField(required=True)
+
+class ActivityStage(Document):
+    date = StringField(required=True)
+    start = StringField(required=True)
+    stop = StringField(required=True)
+    distance = IntField(required=True)
+    calories = IntField(required=True)
+    steps = IntField(required=True)
+
+class HeartrateAuto(Document):
+    date = StringField(required=True)
+    time = StringField(required=True)
+    heart_rate = IntField(required=True)
