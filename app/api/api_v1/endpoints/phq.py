c0247c763a977938d4a77e4a6133a2b2beefc885
Pratik Gorade <pratikgorade0@gmail.com>
2021-04-28 17:30:48 +0530

    endpoints: phq9: initial structure
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- app/api/api_v1/endpoints/phq.py
+++ app/api/api_v1/endpoints/phq.py
@@ -1,14 +1,22 @@
 from fastapi import APIRouter, Depends
+from fastapi.param_functions import Body, 
 
 from app.models.phq import Phq
 from app.api.deps import get_current_user
+from .utils.phqutil import add_answers_to_db, all_questions, three_questions
 
 router = APIRouter()
 
 @router.get("/phq9")
 def phq9_questions(user=Depends(get_current_user)):
-    pass
+    records = Phq.objects(id=user)
+    # if no record exists, send all 9 questions
+    # otherwise send 3 questions
+    if not records:
+        all_questions()
+    else:
+        three_questions(user)
 
 @router.post("/phq9")
-def phq9_score():
-    pass
+def phq9_score(user=Depends(get_current_user), body: dict = Body(...)):
+    add_answers_to_db
---
+++ app/api/api_v1/endpoints/utils/phqutil.py
@@ -0,0 +1,10 @@
+from app.models.user import User
+
+def all_questions() -> list:
+    return []
+
+def three_questions(user: User)  -> list:
+    return []
+
+def add_answers_to_db():
+    pass
