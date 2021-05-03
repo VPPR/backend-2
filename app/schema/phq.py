f7e7bf77c7e945e31f802dbe1630dcbd51d97b7d
Pratik Gorade <pratikgorade0@gmail.com>
2021-05-03 14:31:21 +0530

    schema: phq: change schema and add validators
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- app/schema/phq.py
+++ app/schema/phq.py
@@ -1,7 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class SingleQuestionResponce(BaseModel):
-    qno: int
     score: int
     version: int
+
+    @validator("score")
+    def validate_score_value(score: int):
+        if 0 <= score <= 3:
+            return score
+        else:
+            raise ValueError("score can be integer value from 0 to 3")
+
+    @validator("version")
+    def validate_version_value(version: int):
+        if 1 <= version <= 2:
+            return version
+        else:
+            raise ValueError("version can be either 0 or 1")
