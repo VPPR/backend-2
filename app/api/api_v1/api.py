f03e7e260fb11a7df47c46cb46f8475d9d79de96
Pranav Bakre <pranavbakre@yahoo.in>
2021-05-23 02:05:04 +0530

    removed approval routes
    Signed-off-by: Pranav Bakre <pranavbakre@yahoo.in>

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import approvals, login, phq, user, zip
+from app.api.api_v1.endpoints import login, phq, user, zip
 
 api_router = APIRouter()
 
 
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(user.router, prefix="/users", tags=["user"])
-# api_router.include_router(approvals.router, prefix="/approvals", tags=["Approval"])
 api_router.include_router(zip.router, prefix="/miband", tags=["zip"])
 api_router.include_router(phq.router, prefix="/phq", tags=["phq"])
--- app/models/admin_approval.py
+++
@@ -1,8 +0,0 @@
-from mongoengine import CASCADE, Document
-from mongoengine.fields import ReferenceField
-
-from .user import User
-
-
-class Approval(Document):
-    user = ReferenceField(User, reverse_delete_rule=CASCADE)
