8cc7bbd74dac31e36ef4a40656238c7e40033ef1
Pranav Bakre <pranavbakre@yahoo.in>
2021-06-19 17:49:59 +0530

    added gadgetbridge to upload
    Signed-off-by: Pranav Bakre <pranavbakre@yahoo.in>

--- app/api/api_v1/endpoints/utils/ziputils.py
+++ app/api/api_v1/endpoints/utils/ziputils.py
@@ -1,3 +1,12 @@
+from app.models.gadgetbridge import Gadgetbridge
+import os
+import shutil
+import uuid
+import sqlite3
+from fastapi.datastructures import UploadFile
+from fastapi.exceptions import HTTPException
+from fastapi import status
+import magic
 import pandas
 from pymongo.errors import BulkWriteError
 
@@ -119,5 +128,59 @@ async def activity_stage(df: pandas.DataFrame, user: User):
             "Batch Inserted with some errors. May be some duplicates were found and are skipped."
         )
 
+    except Exception as e:
+        print({"error": str(e)})
+
+
+async def gadgetbridge(sqlite_file: UploadFile, user: User):
+
+    # Check if file uploaded is of type sqlite3
+    if magic.from_buffer(sqlite_file.file.read(), mime=True) != "application/x-sqlite3":
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Wrong File type. Only sqlite file is acceptable",
+        )
+
+    # Create directory gadgetbridge to prevent the main directory from being flooded with sqlite db files
+    if not os.path.exists("gadgetbridge"):
+        os.makedirs("gadgetbridge")
+
+    # Create temp db file
+    filename = "gadgetbridge/" + str(uuid.uuid4()) + ".db"
+    sqlite_file.file.seek(0)
+    try:
+        with open(filename, "wb") as f:
+            shutil.copyfileobj(sqlite_file.file, f)
+    finally:
+        sqlite_file.file.close()
+    con = sqlite3.connect(filename)
+    con.row_factory = sqlite3.Row
+    records = []
+    cur = con.cursor()
+    try:
+        cur.execute("SELECT TIMESTAMP,HEART_RATE FROM MI_BAND_ACTIVITY_SAMPLE")
+        for record in cur:
+            record = dict(record)
+            gadgetbridge = Gadgetbridge(
+                user=user,
+                timestamp=record.get("TIMESTAMP"),
+                heart_rate=record.get("HEART_RATE"),
+            )
+            records.append(gadgetbridge.to_mongo())
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="File doesn't contain required data",
+        )
+    finally:
+        cur.close()
+        con.close()
+        os.remove(filename)
+    try:
+        Gadgetbridge._get_collection().insert_many(records, ordered=False)
+    except BulkWriteError:
+        print(
+            "Batch Inserted with some errors. May be some duplicates were found and are skipped."
+        )
     except Exception as e:
         print({"error": str(e)})
